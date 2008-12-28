921061933cc79600e5f84639dcaa6624caff905f
nitoyon <saita@www2.co-conv.jp>
2008-12-29 00:23:12 +0900

    added from_file option

--- ruby/db/register_bookmarks.rb
+++ ruby/db/register_bookmarks.rb
@@ -1,6 +1,8 @@
+require 'optparse'
 require 'rubygems'
 require 'sqlite3'
 require 'crawler/entry'
+require 'crawler/daily_rank'
 
 $db = SQLite3::Database.new('hatebu_nenkan.db')
 $stu = $db.prepare("select user from bookmarks where entry_id = ?")
@@ -11,15 +13,22 @@ $st_tag_insert = $db.prepare("insert into tags(name) values(?)")
 
 def update_entry(url, entry)
   entry_id, last_update = $db.execute("select id,last_update from entries where url = ?", url).flatten
-  raise "entry data not found: #{url}" if entry_id.nil?
-  return unless last_update.nil?
+  if entry_id.nil? then
+    $db.execute("insert into entries (eid,url,title,last_update)" +
+                "values(?,?,?,?)",
+                entry.bid, url, entry.title,
+                entry.time.strftime("%Y-%m-%d %H:%M:%S"))
+    return $db.last_insert_row_id
+  else
+    return entry_id unless last_update.nil?
 
-  $db.execute("update entries " + 
-             "set eid=?,url=?,title=?,last_update=? where id=?",
-              entry.bid, url, entry.title,
-              entry.time.strftime("%Y-%m-%d %H:%M:%S"),
-              entry_id)
-  return $db.last_insert_row_id
+    $db.execute("update entries " + 
+                "set eid=?,url=?,title=?,last_update=? where id=?",
+                entry.bid, url, entry.title,
+                entry.time.strftime("%Y-%m-%d %H:%M:%S"),
+                entry_id)
+  end
+  return entry_id
 end
 
 # insert tag
@@ -49,17 +58,14 @@ def get_bookmark_users(entry_id)
   return $stu.execute(entry_id).inject({}) { |obj,row| obj[row[0]]=1; obj}
 end
 
-def register(entry_id, url)
-  users = get_bookmark_users(entry_id)
-
+def register_entry(url)
   entry = Entry.new(url)
   entry.load
 
-  cur = Time.now
-  update_entry(url, entry)
+  entry_id = update_entry(url, entry)
+  users = get_bookmark_users(entry_id)
 
   added = false
-  cur = Time.now
   entry.bookmarks.each do |bookmark|
     next if users[bookmark[:id]]
     add_bookmark(entry_id, bookmark)
@@ -69,18 +75,39 @@ def register(entry_id, url)
 end
 
 
-start = (ARGV[0] || "0").to_i
-count = (ARGV[1] || "9999999").to_i
+start = nil
+count = 9999999
+
+opt = OptionParser.new
+opt.on('-s=VAL', '--start_id=VAL', 'Start id') {|v| start = v}
+opt.on(nil, '--from_file', 'Not from db, from file') {|v| from_file = v}
+opt.on('-c=VAL', '--count=VAL', 'Count') {|v| count = v.to_i}
+opt.parse! ARGV
+from_file = !from_file.nil?
+
+
+if from_file then
+  entries = $db.execute("select url from entries where id >= ?;", start).flatten
+else
+  entries = []
+  start = start || "2005-02-10"
+  p start
+  Date.parse(start).upto(Date.today - 1) do |date|
+    rank = DailyRank.new(date.year, date.month, date.day)
+    rank.load
+    entries += rank.entries.map {|entry| entry[:url]}
+  end
+end
+entries = entries.uniq[0 .. count]
 
 i = 0
-$db.execute("select id,url from entries where id >= ?;", start) do |entry|
-  print "#{entry[0]}: #{entry[1]}\t"
+entries.each do |url|
+  i += 1
+  print "#{i}: #{url}\t"
   $stdout.flush
 
   added = false
-  $db.transaction do |db| added = register(entry[0], entry[1]) end
-  #register(entry[0], entry[1])
+  $db.transaction do |db| added = register_entry(url) end
+  #register_entry(url)
   print added ? " OK\n" : " Skip\n"
-  i += 1
-  exit if i >= count
 end
