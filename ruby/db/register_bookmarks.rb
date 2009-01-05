85bd7ff9ef1e6ac6988b0f0a594828edb7ad7db3
nitoyon <nitoyon>
2009-01-06 01:36:52 +0900

    Splitted database per year.

--- ruby/db/register_bookmarks.rb
+++ ruby/db/register_bookmarks.rb
@@ -1,15 +1,22 @@
 require 'optparse'
 require 'rubygems'
 require 'sqlite3'
+require 'db/connect_raw'
 require 'crawler/entry'
 require 'crawler/daily_rank'
 
-$db = SQLite3::Database.new('hatebu_nenkan.db')
-$stu = $db.prepare("select user from bookmarks where entry_id = ?")
-$stb = $db.prepare("insert into bookmarks(entry_id,user,time,commented) values(?,?,?,?)")
-$stt = $db.prepare("insert into bookmarktags(bookmark_id,tag_id) values(?,?)")
-$st_tag_select = $db.prepare("select id from tags where name = ?")
-$st_tag_insert = $db.prepare("insert into tags(name) values(?)")
+def init_db_statement(year)
+  $stt.each_value do |stt| stt.close  end unless $stt.nil?
+  print "open db: #{year}\n"
+  connect_raw(year)
+
+  $stt = {}
+  $stt['users'] = $db.prepare("select user from bookmarks where entry_id = ?")
+  $stt['bookmark'] = $db.prepare("insert into bookmarks(entry_id,user,time,comment) values(?,?,?,?)")
+  $stt['tag'] = $db.prepare("insert into bookmarktags(bookmark_id,tag_id) values(?,?)")
+  $stt['tag_select'] = $db.prepare("select id from tags where name = ?")
+  $stt['tag_insert'] = $db.prepare("insert into tags(name) values(?)")
+end
 
 def update_entry(url, entry)
   entry_id, last_update = $db.execute("select id,last_update from entries where url = ?", url).flatten
@@ -35,30 +42,32 @@ end
 def add_tag(tags, id)
   # bookmark_id, tag
   tags.each do |tag|
-    tag_id = $st_tag_select.execute(tag).next
+    tag_id = $stt['tag_select'].execute(tag).next
     if tag_id.nil? then
-      $st_tag_insert.execute(tag)
+      $stt['tag_insert'].execute(tag)
       tag_id = $db.last_insert_row_id
     else
       tag_id = tag_id.flatten[0]
     end
-    $stt.execute(id, tag_id)
+    $stt['tag'].execute(id, tag_id)
   end
 end
 
 # insert bookmark
 def add_bookmark(entry_id, bookmark)
-  $stb.execute(entry_id, bookmark[:id],
+  $stt['bookmark'].execute(entry_id, bookmark[:id],
                bookmark[:time].strftime("%Y-%m-%d %H:%M:%S"),
-               !bookmark[:desc].nil? && bookmark[:desc] != "")
+               bookmark[:desc])
   add_tag(bookmark[:tags], $db.last_insert_row_id)
 end
 
 def get_bookmark_users(entry_id)
-  return $stu.execute(entry_id).inject({}) { |obj,row| obj[row[0]]=1; obj}
+  return $stt['users'].execute(entry_id).inject({}) do |obj,row|
+    obj[row[0]]=1; obj
+  end
 end
 
-def register_entry(url)
+def register_entry(url, year)
   entry = Entry.new(url)
   entry.load
 
@@ -67,7 +76,7 @@ def register_entry(url)
 
   added = false
   entry.bookmarks.each do |bookmark|
-    next if users[bookmark[:id]]
+    next if users[bookmark[:id]] or bookmark[:time].year != year
     add_bookmark(entry_id, bookmark)
     added = true
   end
@@ -75,39 +84,44 @@ def register_entry(url)
 end
 
 
-start = nil
+start = 2005
 count = 9999999
 
 opt = OptionParser.new
-opt.on('-s=VAL', '--start_id=VAL', 'Start id') {|v| start = v}
-opt.on(nil, '--from_file', 'Not from db, from file') {|v| from_file = v}
+opt.on('-s=VAL', '--start_year=VAL', 'Start year') {|v| start = v}
+opt.on(nil, '--from_file', 'Not from db, from file') {|v| $from_file = v}
 opt.on('-c=VAL', '--count=VAL', 'Count') {|v| count = v.to_i}
 opt.parse! ARGV
-from_file = !from_file.nil?
+$from_file = !$from_file.nil?
 
-
-if from_file then
-  entries = $db.execute("select url from entries where id >= ?;", start).flatten
-else
-  entries = []
-  start = start || "2005-02-10"
-  p start
-  Date.parse(start).upto(Date.today - 1) do |date|
-    rank = DailyRank.new(date.year, date.month, date.day)
-    rank.load
-    entries += rank.entries.map {|entry| entry[:url]}
+def get_entries_in(year)
+  if $from_file then
+    entries = $db.execute(<<SQL, Date.new(year).to_s, Date.new(year + 1).to_s).flatten
+select E.url from dailyranks DR
+    join entries E on DR.entry_id=E.id
+    where DR.date >= date(?) and DR.date < date(?)
+    group by DR.entry_id order by E.id;
+SQL
+  else
+    entries = []
+    Date.new(year).upto(Date.new(year + 1) - 1) do |date|
+      break if date >= Date.today - 1
+      rank = DailyRank.new(date.year, date.month, date.day)
+      rank.load
+      entries += rank.entries.map {|entry| entry[:url]}
+    end
   end
+  entries = entries.uniq
 end
-entries = entries.uniq[0 .. count]
 
-i = 0
-entries.each do |url|
-  i += 1
-  print "#{i}: #{url}\t"
-  $stdout.flush
+start.upto(Date.today.year) do |year|
+  init_db_statement(year)
+  get_entries_in(year).each do |url|
+    print "#{url}\t"
+    $stdout.flush
 
-  added = false
-  $db.transaction do |db| added = register_entry(url) end
-  #register_entry(url)
-  print added ? " OK\n" : " Skip\n"
+    added = false
+    $db.transaction do |db| added = register_entry(url, year) end
+    print added ? "OK\n" : "Skip\n"
+  end
 end
