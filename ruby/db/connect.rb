1fa7eda3f45f4c8ac8bea1e0358c419467dd986c
nitoyon <nitoyon>
2009-01-06 01:34:55 +0900

    Splitted database file per year.

--- ruby/db/connect.rb
+++ ruby/db/connect.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-                                        :adapter => 'sqlite3',
-                                        :dbfile => 'hatebu_nenkan.db'
-                                        )
+def connect(year)
+  ActiveRecord::Base.remove_connection if ActiveRecord::Base.connected?
+  ActiveRecord::Base.establish_connection(
+                                          :adapter => 'sqlite3',
+                                          :dbfile => "hatebu_nenkan#{year.to_s}.db"
+                                          )
+end
--- ruby/db/connect_raw.rb
+++ ruby/db/connect_raw.rb
@@ -1,3 +1,7 @@
 require 'sqlite3'
 
-$db = SQLite3::Database.new('hatebu_nenkan.db')
+def connect_raw(year)
+  p $db.nil?
+  $db.close if !$db.nil? && !$db.closed?
+  $db = SQLite3::Database.new("hatebu_nenkan#{year.to_s}.db")
+end
