16fe21c8004a17a56255efee60682723a8fcba4a
nitoyon <nitoyon>
2009-02-01 16:53:25 +0900

    Read last modified date from db.

--- ruby/db/register_bookmarks.rb
+++ ruby/db/register_bookmarks.rb
@@ -4,6 +4,7 @@ require 'sqlite3'
 require 'db/connect_raw'
 require 'crawler/entry'
 require 'crawler/daily_rank'
+require 'time'
 
 def init_db_statement(year)
   $stt.each_value do |stt| stt.close  end unless $stt.nil?
@@ -27,8 +28,7 @@ def update_entry(url, entry)
                 entry.time.strftime("%Y-%m-%d %H:%M:%S"))
     return $db.last_insert_row_id
   else
-    return entry_id unless last_update.nil?
-
+    #return entry_id unless last_update.nil?
     $db.execute("update entries " + 
                 "set eid=?,url=?,title=?,last_update=? where id=?",
                 entry.bid, url, entry.title,
@@ -86,16 +86,23 @@ end
 
 start = 2005
 count = 9999999
+modified = Time.at(0)
 
 opt = OptionParser.new
-opt.on('-s=VAL', '--start_year=VAL', 'Start year') {|v| start = v}
+opt.on('-s=VAL', '--start_year=VAL', 'Start year') {|v| start = v.to_i}
 opt.on(nil, '--from_file', 'Not from db, from file') {|v| $from_file = v}
 opt.on('-c=VAL', '--count=VAL', 'Count') {|v| count = v.to_i}
+opt.on('-m=VAL', '--modified=VAL', 'If modified since') {|v| modified = Time.parse(v)}
 opt.parse! ARGV
 $from_file = !$from_file.nil?
 
+print("count:      #{count}\n")
+print("start year: #{start}\n")
+print("modified:   #{modified.to_s}\n")
+print("\n")
+
 def get_entries_in(year)
-  if $from_file then
+  if !$from_file then
     entries = $db.execute(<<SQL, Date.new(year).to_s, Date.new(year + 1).to_s).flatten
 select E.url from dailyranks DR
     join entries E on DR.entry_id=E.id
@@ -116,12 +123,24 @@ end
 
 start.upto(Date.today.year) do |year|
   init_db_statement(year)
+
   get_entries_in(year).each do |url|
+    _start = Time.now
+
     print "#{url}\t"
     $stdout.flush
+
+    last_update = $db.execute("select last_update from entries where url = ?", url).flatten[0]
+    if !last_update.nil? && Time.parse(last_update) > modified then
+      print "Skip\n"
+      next
+    end
 
     added = false
     $db.transaction do |db| added = register_entry(url, year) end
     print added ? "OK\n" : "Skip\n"
+
+    _end = Time.now
+    sleep([5 - (_end - _start), 0].max)
   end
 end
